7ed8e4732ace224967a527b8c7ecbe2ee621e60b
ibrahimaDev <jallodevfereya@gmail.com>
2019-03-31 21:55:27 +0200

    ajout de calcul d'heritage

---
+++ HeritageProjetGrp6/Controllers/CalculController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeritageProjetGrp6.Controllers
+{
+    public class CalculController : Controller
+    {
+        // GET: Calcul
+        public ActionResult Calcul()
+        {
+            return View();
+        }
+        // GET: CalculFemme
+        public ActionResult CalculFemme()
+        {
+            return View();
+        }
+        // GET: info
+        public ActionResult info()
+        {
+            return View();
+        }
+
+
+    }
+}
